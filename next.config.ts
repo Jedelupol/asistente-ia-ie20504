96b9d93abc6de2b50ccefec02b1c014c6fce8583
Jedelupol <javierruth123@gmail.com>
2026-03-15 17:23:54 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-ignore - allowedDevOrigins is present in Next.js but might be missing from types
-  experimental: {
-    allowedDevOrigins: ["192.168.0.103:3000", "192.168.0.103"],
-  },
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**' },
-      { protocol: 'http', hostname: '**' },
+      {
+        protocol: 'https',
+        hostname: 'v0.blob.com',
+      },
     ],
   },
 };
